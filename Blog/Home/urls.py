881993f6ab62807bd687104ea5574ad20efff032
GoGei <rich.290401@gmail.com>
2022-03-11 11:17:34 +0200

    Added basic pages and urls

--- Blog/Home/urls.py
+++ Blog/Home/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='home-index'),
+    url(r'^contacts/$', views.contacts, name='home-contacts'),
+    url(r'^about/$', views.about, name='home-about'),
+    url(r'^posts/$', views.posts, name='home-posts'),
 ]
--- Blog/Home/views.py
+++ Blog/Home/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'Home/index.html')
+
+
+def posts(request):
+    return render(request, 'Home/posts.html')
+
+
+def contacts(request):
+    return render(request, 'Home/contacts.html')
+
+
+def about(request):
+    return render(request, 'Home/about.html')
