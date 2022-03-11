db6cba23479c47013a8f059ac2105a6e88427eac
GoGei <rich.290401@gmail.com>
2022-03-11 12:01:52 +0200

    Set postgres database as default

--- settings.py
+++ settings.py
@@ -87,8 +87,13 @@ WSGI_APPLICATION = 'wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR + 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'ATOMIC_REQUESTS': True,
     }
 }
 
