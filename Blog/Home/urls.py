bdc8efc1501e2bcaf0c1e06e59835fbf130068f1
GoGei <rich.290401@gmail.com>
2022-03-26 19:55:35 +0200

    Commit

--- Blog/Home/urls.py
+++ Blog/Home/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^posts/$', views.home_posts_view, name='home-posts'),
     url(r'^recently-changed-posts/$', views.home_posts_view, kwargs={'extra_filter': PostClassifier.RECENTLY_CHANGED}, name='home-recently-changed-posts'),
     url(r'^this-month-posts/$', views.home_posts_view, kwargs={'extra_filter': PostClassifier.THIS_MONTH}, name='home-this-month-posts'),
-    url(r'^post/(?P<post_slug>\w+)/$', views.home_post_view, name='home-post-view'),
+    url(r'^post/(?P<post_slug>[\w\W\-]+)/$', views.home_post_view, name='home-post-view'),
 ]
